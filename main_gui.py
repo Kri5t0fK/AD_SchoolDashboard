0cfa1b1ef407b7e718eea10127ce2015f2180c6d
Kri5t0f (Krzysztof Kordal) <53398204+Kri5t0fK@users.noreply.github.com>
2023-06-23 00:27:02 +0200

    Student Tab started: comboBoxes Done

--- main_gui.py
+++ main_gui.py
@@ -35,12 +35,23 @@ class GUI_MainWindow(qtw.QMainWindow, Ui_MainWindow):
         # Create a database connection
         self.db_connection = db_func.create_connection(self.DATABASE_PATH)
         self.cursor = self.db_connection.cursor()
+
+        # Prepare data for comboBoxes
+        self._prepare_student_student_comboBox()
+
         # Connect buttons
         self.button_general_refresh.clicked.connect(self._refresh_general_plots)
+        # Connect comboBoxes to autorefresh
+        self.input_comboBox_student_student.currentIndexChanged.connect(self._refresh_from_student_student)
+        self.input_comboBox_student_subject.currentIndexChanged.connect(self._refresh_student_plots)
+
+
+
 
 
         # Plot and Refresh plots on start
         self._refresh_general_plots()
+        self._refresh_student_plots()
 
     
     def _refresh_general_plots(self):
@@ -87,6 +98,42 @@ class GUI_MainWindow(qtw.QMainWindow, Ui_MainWindow):
         self.mpl_widget_general_4.axis.set(title='Liczba uczniów w poszczgólnych klasach')
         self.mpl_widget_general_4.canvas.draw()
     
+    def _refresh_from_student_student(self):
+        self._prepare_student_subject_comboBox()
+        self._refresh_student_plots()
+    
+    def _prepare_student_student_comboBox(self):
+        # Get list of all students
+        self.cursor.execute("SELECT ID_Student, name, surname FROM Students ORDER BY name ASC, surname ASC")
+        self.students_list = np.array(self.cursor.fetchall())
+
+        # Make list of concatenated [name+surname]
+        full_names_list = [f'{name} {surname}' for name, surname in self.students_list[:, 1:3]]
+
+        # Prepare student comboBox
+        self.input_comboBox_student_student.clear()
+        self.input_comboBox_student_student.addItems(full_names_list)
+
+    def _prepare_student_subject_comboBox(self):
+        # Get current selected student ID
+        comboBox_index = self.input_comboBox_student_student.currentIndex()
+        current_student_ID = self.students_list[comboBox_index, 0]
+
+        # Fetch all subjects for given student
+        self.cursor.execute(f"SELECT Subjects.ID_subject, Subjects.subject_name FROM Subjects INNER JOIN Grades ON Grades.ID_subject=Subjects.ID_subject INNER JOIN Students ON Students.ID_Student=Grades.ID_student WHERE Students.ID_Student={current_student_ID} GROUP BY Subjects.ID_subject ORDER BY subject_name ASC")
+        self.student_subjects_list = np.array(self.cursor.fetchall())
+
+        # Make list of concatenated [name+surname]
+        subject_names_list = [f'{name}' for name in self.student_subjects_list[:, 1]]
+
+
+        # # Prepare student comboBox
+        self.input_comboBox_student_subject.clear()
+        self.input_comboBox_student_subject.addItems(subject_names_list)
+    
+    def _refresh_student_plots(self):
+        pass
+
 
 if __name__ == "__main__":
     import sys
